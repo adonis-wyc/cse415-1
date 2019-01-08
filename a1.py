64db922d758e74adffdc9115f84571debf4be221
kuo22 <kuohong22@gmail.com>
2019-01-08 01:52:17 -0800

    part 1

--- a1.py
+++ a1.py
@@ -1,3 +1,10 @@
+irregular = {
+    'have': 'had',
+    'be': 'was',
+    'eat': 'ate',
+    'go': 'went'
+}
+
 def five_x_cubed_plus_1(n):
     return 5 * (n ** 3) + 1
 
@@ -10,7 +17,37 @@ def pair_off(n):
     return pairs
 
 def mystery_code(n):
-    return "hi"
+    output = ''
+    for c in n:
+        char = c
+        if c.isalpha():
+            if c.upper() <= 'G':
+                if c.islower():
+                    char = chr(ord(c.upper()) + 19).upper()
+                else:
+                    char = chr(ord(c) + 19).lower()
+            else:
+                if c.islower():
+                    char = chr(ord(c.upper()) - 7).upper()
+                else:
+                    char = chr(ord(c) - 7).lower()
+        output += char
+    return output
 
+def past_tense(n):
+    verbs = []
+    for verb in n:
+        verbs.append(irregular.get(verb, other_verbs(verb)))
+    return verbs
 
-print(pair_off([1, 2, 3, 4, 5, 'a', 'b', ['x', 'y'], ['z'], 'second from last', 'last']))
+def other_verbs(n):
+    if n[-1] == 'e':
+        return n + 'd'
+    elif n[-1] == 'y' and n[-2] not in 'aeiou':
+        return n[:-1] + 'ied'
+    elif n[-1] not in 'aeiouyw' and n[-2] in 'aeiou' and n[-3] not in 'aeiou':
+        return n + n[-1] + 'ed'
+    else:
+        return n + 'ed'
+
+print(past_tense(['program', 'debug', 'execute', 'crash', 'repeat', 'eat']))
