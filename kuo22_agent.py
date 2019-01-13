562de776cd1487689a83ca6595913e0ae429f009
kuo22 <kuohong22@gmail.com>
2019-01-12 21:47:09 -0800

    adds comments

--- kuo22_agent.py
+++ kuo22_agent.py
@@ -1,7 +1,6 @@
 from random import choice
 from re import *
 
-
 def introduce():
     return """I am Shrek.
         I am a mean green ogre. You better stop bothering me before I send you to my belly.
@@ -28,39 +27,54 @@ def respond(the_input):
     global gender
     global math
 
+    # Asks others to same something if input is empty
     if wordlist[0] == '':
         return "Don't just stare at me.  Say something!"
+    # Gives the location of home when asked "where do you live"
     if wordlist[0:4] == ['where', 'do', 'you', 'live']:
         return "I live in a beautiful swamp where I enjoy every day of my life."
+    # Returns randomly generated feeling when asked "how are you"
     if wordlist[0:3] == ['how', 'are', 'you']:
         feeling = choice(['happy', 'sad', 'angry', 'mad', 'anxious', 'content'])
         return "I feel " + feeling + " right now."
+    # Returns an encouraging agreement to responses that start with "I will"
     if wordlist[0:2] == ['i', 'will'] and len(wordlist) > 2:
         return "Interesting.  I hope you " + stringify(mapped_wordlist[2:]) + " too."
+    # Returns aggressive response when asked where something is
     if wordlist[0:2] == ['where', 'is']:
         return "What makes you think I know?  Go find it yourself."
+    # Returns the next verse after "Shrek is love"
     if wordlist[0:3] == ['shrek', 'is', 'love']:
         return "Shrek is life."
+    # Returns a mean remark when the response contains "bored"
     if 'bored' in wordlist:
         return "Why don't you get outta here and find something to do."
+    # Returns annoyed comment when response mentions "donkey"
     if 'donkey' in wordlist:
         return "Oh, don't remind me of that guy again."
+    # Remembers the gender of the other agent if response mentions "boy" and the question "are you a boy or a girl" has been asked
     if 'boy' in wordlist and asked == True and gender == '':
         gender = 'boy'
         return "Okay, I will remember that."
+    # Remembers the gender of the other agent if response mentions "girl" and the question "are you a boy or a girl" has been asked
     if 'girl' in wordlist and asked == True and gender == '':
         gender = 'girl'
         return "Okay, I will remember that."
+    # Returns a prideful remark when response mentions "swamp"
     if 'swamp' in wordlist:
         return "Yes, my swamp is very beautiful."
+    # Returns a retaliation when response mentions "fear"
     if 'fear' in wordlist:
         return "I ain't afraid of you!"
+    # Sings a verse from the song All Star when certain music keywords are mentioned
     for word in ['star', 'sing', 'song']:
         if word in wordlist:
             return sing()
+    # Returns a depressing reply if "princess" or "fiona" is mentioned
     for word in ['princess', 'fiona']:
         if word in wordlist:
             return "Oh, I miss her. Things have not been the same ever since she left me."
+    # Compatible with my partner's agent. Asks other agent to stop talking about math if the response contains certain math terms.
     for word in ['graph', 'vertices', 'epsilon', 'topology']:
         if word in wordlist:
             if not math:
@@ -68,9 +82,10 @@ def respond(the_input):
                 return "Stop talking about math, I don't know what you mean."
             else:
                 return choice(["Stop with your nerdy nonsense! Leave today or I will cook you for dinner.", "I am really mad now!"])
-    
+    # Returns an annoyed comment if response starts with a questioning word.
     if wpred(wordlist[0]):
         return "I am not an encyclopedia.  Stop bothering me!"
+    # Starts by asking if other agent is a boy or a girl. Then cycles through various insults if no other rules trigger.
     else:
         if (asked == False):
             asked = True
@@ -113,12 +128,14 @@ LYRIC = ['Somebody once told me the world is gonna roll me', "I ain't the sharpe
 OTHER = ['I have no idea what you are talking about', 'Stop speaking gibberish', 'I will knock you out', "I ain't got all day", 'Speak up! Do you fear me']
 
 count = 0
+# Cycles through a few insults
 def other():
     global count
     count += 1
     return OTHER[count % 5]
 
 lyric_count = -1
+# Cycles through the first few lines of All Star
 def sing():
     global lyric_count
     lyric_count += 1
